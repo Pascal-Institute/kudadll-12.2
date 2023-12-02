07e929a536b256a023184ca34c01b17377f3a05f
volta2030 <volta2030@gmail.com>
2023-12-02 12:18:40 +0900

    -Add init()

--- kuda_driver_api.cpp
+++ kuda_driver_api.cpp
@@ -1,1 +1,10 @@
 #include "kuda_driver_api.h"
+#include <jni.h>
+#include <cuda.h>
+
+JNIEXPORT jint JNICALL Java_kuda_DriverAPI_init(JNIEnv* env, jobject obj, jint flags) {
+	
+	CUresult cudaStatus = cuInit((unsigned int) flags);
+
+	return cudaStatus;
+}
--- kuda_driver_api.h
+++ kuda_driver_api.h
@@ -1,1 +1,13 @@
-#pragma once
+#include <jni.h>
+
+//https://docs.nvidia.com/cuda/cuda-driver-api/index.html
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+	//6.3 Initialization
+	JNIEXPORT jint JNICALL Java_kuda_DriverAPI_init(JNIEnv* env, jobject obj, jint flags);
+
+#ifdef __cplusplus
+}
+#endif
