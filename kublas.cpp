d3844c7c6d300a3e6233fe789e50abac2e67f751
volta2030 <volta2030@gmail.com>
2023-12-31 21:34:47 +0900

    -Modify create(), destroy()

--- kublas.cpp
+++ kublas.cpp
@@ -1,11 +1,11 @@
 #include "kublas.h"
 #include <jni.h>
-#include <cublas.h>
+#include <cublas_v2.h>
 
 JNIEXPORT jlong JNICALL Java_kuda_Kublas_create(JNIEnv* env, jobject obj) {
 	cublasHandle_t handle;
 
-	cublasStatus_t cublasStatus = cublasCreate_v2(&handle);
+	cublasStatus_t cublasStatus = cublasCreate(&handle);
 	
 	if (cublasStatus != CUBLAS_STATUS_SUCCESS) {
 		return cublasStatus;
@@ -18,7 +18,7 @@ JNIEXPORT jint JNICALL Java_kuda_Kublas_destroy(JNIEnv* env, jobject obj, jlong 
 	
 	cublasHandle_t cublasHandle = reinterpret_cast<cublasHandle_t>(handle);
 
-	cublasStatus_t cublasStatus = cublasDestroy_v2(cublasHandle);
+	cublasStatus_t cublasStatus = cublasDestroy(cublasHandle);
 
 	return cublasStatus;
 }
